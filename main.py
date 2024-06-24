6ba2cd6d21a09099263115d50e44e93ea68e77e9
Samangelof <ksa1sano@mail.ru>
2024-06-24 23:22:32 +0300

    edit keywords requests | 3(150) page scrap

--- main.py
+++ main.py
@@ -1,65 +1,16 @@
 import tkinter as tk
 from tkinter import simpledialog
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import urllib.parse
 import queue
-from categories import categories
+import json
 import threading
-
-
-class WildberriesFrequentSearchScraper:
-    def __init__(self, url):
-        self.url = url
-        self.driver = None
-
-    def setup_driver(self):
-        service = Service(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=service)
-
-    def open_page(self):
-        self.driver.get(self.url)
-        time.sleep(1)
-
-    def click_search_input(self):
-        search_input = self.driver.find_element(By.CSS_SELECTOR, 'input#searchInput')
-        search_input.click()
-
-    def get_frequent_searches(self):
-        frequent_searches = []
-        autocomplete_list = self.driver.find_element(By.CSS_SELECTOR, 'ul.autocomplete__list.autocomplete__list--catalog')
-        suggestion_items = autocomplete_list.find_elements(By.CSS_SELECTOR, 'li.autocomplete__item.j-suggest')
-        
-        for item in suggestion_items:
-            phrase = item.find_element(By.CSS_SELECTOR, 'span.autocomplete__phrase').text
-            frequent_searches.append(phrase)
-            
-        return frequent_searches
-
-
-    def scrape_frequent_searches(self):
-        try:
-            self.setup_driver()
-            self.open_page()
-            
-            self.click_search_input()
-            
-            time.sleep(1)
-            
-            frequent_searches = self.get_frequent_searches()
-            print("Частые запросы:")
-            for i, search in enumerate(frequent_searches, start=1):
-                print(f"{i}. {search}")
-                
-        finally:
-            self.close_driver()
-
-    def close_driver(self):
-        self.driver.quit()
-
+from categories import categories
 
 
 class WildberriesScraper:
@@ -69,27 +20,35 @@ class WildberriesScraper:
         self.driver = None
 
     def setup_driver(self):
+        options = Options()
+        # скрыть браузер
+        options.add_argument('--headless')
         service = Service(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=service)
+        self.driver = webdriver.Chrome(service=service, options=options)
 
     def open_page(self, url):
+        print(f"Открытие страницы: {url}")
         self.driver.get(url)
         time.sleep(10)
 
     def get_total_goods(self):
+        print("Получение общего количества товаров...")
         total_goods_element = self.driver.find_element(By.CSS_SELECTOR, 'div.total-goods[data-tag="totalGoods"]')
         total_goods_text = total_goods_element.text
         return total_goods_text
 
     def enter_keywords_on_second_page(self, keywords):
+        print("Ввод ключевых слов на второй странице...")
         search_area = self.driver.find_element(By.CSS_SELECTOR, 'textarea#search_area')
         search_area.send_keys(keywords)
-        
+
     def click_search_button(self):
+        print("Нажатие кнопки поиска...")
         search_button = self.driver.find_element(By.CSS_SELECTOR, 'button.btn.btn-primary[type="submit"]')
         search_button.click()
 
     def get_keyword_counts(self, user_keywords):
+        print("Получение количества ключевых слов...")
         keyword_counts = {}
         rows = self.driver.find_elements(By.CSS_SELECTOR, 'div.container table tr')[1:]
         for row in rows:
@@ -102,24 +61,48 @@ class WildberriesScraper:
                 keyword_counts[keyword] = (count, benefit, category)
         return keyword_counts
 
+    def get_all_keywords(self, page=1):
+        print("Получение всех ключевых слов...")
+        all_keywords = []
+        while page <= 3:  
+            print(f"Открытие страницы {page}...")
+            self.open_page(f"{self.second_url}?search=&page={page}")
+            time.sleep(5)
+            rows = self.driver.find_elements(By.CSS_SELECTOR, 'div.container table tr')[1:]
+            if not rows:
+                break
+            for row in rows:
+                cells = row.find_elements(By.TAG_NAME, 'td')
+                keyword = cells[0].text
+                count = cells[1].text
+                benefit = cells[4].text
+                all_keywords.append((keyword, count, benefit))
+            next_button = self.driver.find_elements(By.CSS_SELECTOR, 'div.pages div.page.next a')
+            if not next_button or 'следующая' not in next_button[0].text:
+                break
+            page += 1
+        return all_keywords
 
     def close_driver(self):
+        print("Закрытие браузера...")
         self.driver.quit()
 
     def scrape(self, keywords):
+        start_time = time.time()
         try:
+            print("Настройка драйвера...")
             self.setup_driver()
             search_url = self.generate_search_url(keywords)
             self.open_page(search_url)
             total_goods_text = self.get_total_goods()
             print(f'Количество товаров (конкурентов): {total_goods_text}')
-            
+
             self.open_page(self.second_url)
             self.enter_keywords_on_second_page(keywords)
             self.click_search_button()
-            
+
             time.sleep(5)
-            
+
             keyword_counts = self.get_keyword_counts(keywords)
             if keyword_counts:
                 print("Результаты поиска:")
@@ -131,10 +114,11 @@ class WildberriesScraper:
                     print()
             else:
                 print("По вашему запросу ничего не найдено.")
-            
+
         finally:
             self.close_driver()
-
+            end_time = time.time()
+            print(f"Время выполнения скрипта: {end_time - start_time:.2f} секунд")
 
     def generate_search_url(self, keywords):
         query = urllib.parse.urlencode({'search': keywords})
@@ -145,6 +129,31 @@ class WildberriesScraper:
             if pattern.search(keyword):
                 return category
         return 'другое'
+
+    def scrape_all_keywords(self):
+        start_time = time.time()
+        try:
+            self.setup_driver()
+            all_keywords = self.get_all_keywords()
+            if all_keywords:
+                print("Все ключевые слова:")
+                data = []
+                for i, (keyword, count, benefit) in enumerate(all_keywords, start=1):
+                    print(f"{i}. Ключевое слово: {keyword}, Количество запросов: {count}, Выгода: {benefit}")
+                    data.append({
+                        'Ключевое слово': keyword,
+                        'Количество запросов': count,
+                        'Выгода': benefit
+                    })
+                with open('all_keywords.json', 'w', encoding='utf-8') as f:
+                    json.dump(data, f, ensure_ascii=False, indent=4)
+                print("Данные сохранены в all_keywords.json")
+            else:
+                print("Ключевые слова не найдены.")
+        finally:
+            self.close_driver()
+            end_time = time.time()
+            print(f"Время выполнения скрипта: {end_time - start_time:.2f} секунд")
 
 
 def on_search():
@@ -157,13 +166,13 @@ def on_search():
 
 def threaded_search():
     keywords_queue = queue.Queue()
-    
+
     def get_keywords():
         keywords = simpledialog.askstring("Input", "Введите ключевые слова для поиска:")
         keywords_queue.put(keywords)
-    
+
     root.after(0, get_keywords)
-    
+
     def wait_for_keywords():
         try:
             keywords = keywords_queue.get_nowait()
@@ -174,16 +183,16 @@ def threaded_search():
                 print("Вы не ввели ключевые слова.")
         except queue.Empty:
             root.after(100, wait_for_keywords)
-    
+
     root.after(100, wait_for_keywords)
 
-def start_frequent_search_scraper():
-    scraper = WildberriesFrequentSearchScraper('https://www.wildberries.ru/')
-    threading.Thread(target=scraper.scrape_frequent_searches).start()
+def start_all_keywords_scraper():
+    scraper = WildberriesScraper(base_url, second_url)
+    threading.Thread(target=scraper.scrape_all_keywords).start()
 
 def close_app():
     root.destroy()
-    
+
 
 if __name__ == "__main__":
     base_url = 'https://global.wildberries.ru/catalog'
@@ -191,29 +200,29 @@ if __name__ == "__main__":
 
     root = tk.Tk()
     root.title("Операции")
-    root.geometry("250x230")
+    root.geometry("250x290")
 
     search_button = tk.Button(
-        root, 
-        text="Начать поиск", 
-        command=threaded_search, 
-        bg="blue", 
-        fg="white", 
-        width=20, 
+        root,
+        text="Начать поиск",
+        command=threaded_search,
+        bg="blue",
+        fg="white",
+        width=20,
         height=2,
         font=('Helvetica', 12, 'bold'))
-    search_button.pack(pady=20)
+    search_button.pack(pady=10)
 
-    frequent_search_button = tk.Button(
-        root, 
-        text="Частые запросы", 
-        command=start_frequent_search_scraper, 
-        bg="yellow", 
-        fg="black", 
-        width=20, 
+    all_keywords_button = tk.Button(
+        root,
+        text="Все ключевые слова",
+        command=start_all_keywords_scraper,
+        bg="yellow",
+        fg="black",
+        width=20,
         height=2,
         font=('Helvetica', 12, 'bold'))
-    frequent_search_button.pack(pady=10)
+    all_keywords_button.pack(pady=10)
 
     close_button = tk.Button(root, text="Закрыть приложение", command=close_app, bg="red", fg="white", width=20, height=2, font=('Helvetica', 12, 'bold'))
     close_button.pack(pady=10)
